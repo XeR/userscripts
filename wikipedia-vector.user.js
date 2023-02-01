66a0654e36adfe07290deb25b4abb0251c05e2a1
XeR <XeR@users.noreply.github.com>
2023-02-01 12:10:24 -0100

    Wikipedia: fix history loop

--- wikipedia-vector.user.js
+++ wikipedia-vector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Wikipedia skin changer
 // @description Forces Wikipedia to use the vector skin
-// @version     1
+// @version     2
 // @namespace   https://xer.fr/userscripts
 // @run-at      document-start
 // @match       https://*.wikipedia.org/*
@@ -11,6 +11,13 @@
 const params = new URLSearchParams(location.search);
 
 if(!params.get("useskin")) {
+	// Update the parameters
 	params.append("useskin", "vector");
-	location.search = params;
+
+	// Update the URL
+	const url  = new URL(location)
+	url.search = params;
+
+	// location.replace does not create a new history element
+	location.replace(url);
 }
